0d71391510bc0d7e0ad88bd4d1a57b25b581538f
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-04-23 20:24:32 +0530

    11- SignIn/SignUp using firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,11 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -13,10 +18,47 @@ const Login = () => {
   const handleButtonClick = () => {
     const message = checkValidData(
       email.current.value,
-      password.current.value,
-      name.current.value
+      password.current.value
+      // name.current.value
     );
     setErrMessage(message);
+    if (message) return;
+
+    // Sign In / Sign Up
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMessage(errorCode + "-" + errorMessage);
+        });
+    }
   };
 
   const toggleSignInForm = () => {
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -5,7 +5,10 @@ export const checkValidData = (email, password, name) => {
   const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(
     password
   );
-  const isNameValid = /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/.test(name);
+  // const isNameValid = /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/.test(name);
+  const isNameValid = /^[a-zA-Z]+([ \-']{0,1}[a-zA-Z]+){0,2}[.]{0,1}$/.test(
+    name
+  );
 
   if (!isNameValid) {
     return "Name is not valid";
