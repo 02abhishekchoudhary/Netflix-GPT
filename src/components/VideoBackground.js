2d02c366739fd3dde3e03a3bd0630bbf51ae64c4
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-05-04 00:13:47 +0530

    25- Trailer fetched, custom hooks, trailer added to store

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,36 +1,16 @@
-import { useEffect, useState } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
-  // const [trailerKey, setTrailerKey] = useState(null);
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1233069/videos",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-    const filterData = json.results.filter(
-      (videos) => videos.type === "Trailer"
-    );
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    // setTrailerKey(trailer.key);
-    dispatch(addTrailerVideo(trailer));
-  };
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  useMovieTrailer(movieId);
 
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
   return (
     <div>
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
