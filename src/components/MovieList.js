95e4f2de22d19c9d4bdc8ef1f529d407c0edcd1e
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-05-18 21:18:46 +0530

    44- Netflix AI done

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -61,37 +61,3 @@ const MovieList = ({ title, movies }) => {
 };
 
 export default MovieList;
-
-// import { useRef } from "react";
-// import MovieCard from "./MovieCard";
-
-// const MovieList = ({ title, movies }) => {
-//   const scrollContainerRef = useRef(null);
-
-//   const handleWheel = (e) => {
-//     if (scrollContainerRef.current) {
-//       e.preventDefault();
-//       scrollContainerRef.current.scrollLeft += e.deltaY;
-//     }
-//   };
-//   if (!movies || movies.length === 0) return null;
-
-//   return (
-//     <div className="px-6">
-//       <h1 className="text-3xl py-4 text-white">{title}</h1>
-//       <div
-//         className="flex overflow-x-scroll hide-scrollbar"
-//         ref={scrollContainerRef}
-//         onWheel={handleWheel}
-//       >
-//         <div className="flex">
-//           {movies?.map((movie) => (
-//             <MovieCard key={movie.id} posterPath={movie.poster_path} />
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MovieList;
--- src/utils/openai.js
+++
@@ -1,8 +0,0 @@
-// import OpenAI from "openai";
-
-// const Openai = new OpenAI({
-//   apiKey: process.env.OPENAI_KEY,
-//   dangerouslyAllowBrowser: true,
-// });
-
-// export default Openai;
